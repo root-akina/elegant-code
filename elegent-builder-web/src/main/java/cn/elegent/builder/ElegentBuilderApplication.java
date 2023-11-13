cc413aad4e8612a35841e30ef2918c6e1dadcc95
娄迎新 <louyingxin@itcast.cn>
2023-11-13 15:34:24 +0800

    修复模板bug

--- elegent-builder-web/src/main/java/cn/elegent/builder/ElegentBuilderApplication.java
+++ elegent-builder-web/src/main/java/cn/elegent/builder/ElegentBuilderApplication.java
@@ -1,7 +1,6 @@
 package cn.elegent.builder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ElegentBuilderApplication {
 
